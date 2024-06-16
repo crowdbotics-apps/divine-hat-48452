43915182019bf3fbcc2aff79f4b18d1a80b7d797
Crowdbotics <team@crowdbotics.com>
2024-06-16 12:01:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-hat-48452",
+  url: "https://divine-hat-48452.botics.co",
+  api: "https://divine-hat-48452.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
